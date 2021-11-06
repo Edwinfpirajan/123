0b4ae388ac09bf06cc2cb387fb305e7bab8e60d8
Mi computador <efpa1998@hotmail.com>
2021-11-06 10:37:48 -0500

    first comment

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export * from './ping.controller';
+export * from './usario.controller';
+export * from './servicio-cliente.controller';
+export * from './servicio-encomienda.controller';
---
+++ src/repositories/servicio.repository.ts
@@ -0,0 +1,27 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, BelongsToAccessor} from '@loopback/repository';
+import {MongoDataSource} from '../datasources';
+import {Servicio, ServicioRelations, Cliente, Encomienda} from '../models';
+import {ClienteRepository} from './cliente.repository';
+import {EncomiendaRepository} from './encomienda.repository';
+
+export class ServicioRepository extends DefaultCrudRepository<
+  Servicio,
+  typeof Servicio.prototype.id,
+  ServicioRelations
+> {
+
+  public readonly origenFk: BelongsToAccessor<Cliente, typeof Servicio.prototype.id>;
+
+  public readonly encomiendaFk: BelongsToAccessor<Encomienda, typeof Servicio.prototype.id>;
+
+  constructor(
+    @inject('datasources.Mongo') dataSource: MongoDataSource, @repository.getter('ClienteRepository') protected clienteRepositoryGetter: Getter<ClienteRepository>, @repository.getter('EncomiendaRepository') protected encomiendaRepositoryGetter: Getter<EncomiendaRepository>,
+  ) {
+    super(Servicio, dataSource);
+    this.encomiendaFk = this.createBelongsToAccessorFor('encomiendaFk', encomiendaRepositoryGetter,);
+    this.registerInclusionResolver('encomiendaFk', this.encomiendaFk.inclusionResolver);
+    this.origenFk = this.createBelongsToAccessorFor('origenFk', clienteRepositoryGetter,);
+    this.registerInclusionResolver('origenFk', this.origenFk.inclusionResolver);
+  }
+}
